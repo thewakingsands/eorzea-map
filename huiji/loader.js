8e61e3c3db8b582300933a4f6bc6fdbb19dd021f
oott123 <git@public.oott123.com>
2018-09-27 00:14:07 +0800

    adjust width

--- huiji/loader.js
+++ huiji/loader.js
@@ -58,8 +58,18 @@
   }
 
   function initMap(eorzeaMap) {
-    $mapContainer = $('<section class="erozea-map-outer"><div class="eorzea-map-glass"></div><div class="eorzea-map-inner"></div>')
-    $mapContainer.appendTo('body');
+    $mapContainer = $('<section class="erozea-map-outer"><div class="eorzea-map-glass"></div><div class="eorzea-map-move-handler"></div><div class="eorzea-map-inner"></div>')
+    if (localStorage && localStorage.YZWFEorzeaMapPos) {
+      var pos = localStorage.YZWFEorzeaMapPos.split(',')
+      if (pos.length === 2) {
+        $mapContainer.css({
+          top: pos[0] + 'px',
+          left: pos[1] + 'px'
+        })
+      }
+    }
+    $mapContainer.appendTo('body')
+    mapMover($mapContainer.find('.eorzea-map-move-handler'), $mapContainer)
     eorzeaMap.create($mapContainer.find('.eorzea-map-inner')[0])
     .then(function (mapInstance) {
       $mapContainer.css({
@@ -77,6 +87,45 @@
   function loadMap(mapId, x, y) {
     map.loadMapKey(mapId)
     $mapContainer.show()
+  }
+
+  function mapMover($handler, $container) {
+    var isDragging = false
+    var startX, startY
+    $handler.on('mousedown pointerdown touchdown', function (event) {
+      event.preventDefault()
+      isDragging = true
+      startX = event.clientX
+      startY = event.clientY
+    })
+    $(window).on('mousemove pointermove touchmove', function (event) {
+      if (!isDragging) {
+        return
+      }
+      event.preventDefault()
+      var diffX = event.clientX - startX
+      var diffY = event.clientY - startY
+      var translate = '(' + diffX + 'px, ' + diffY + 'px, 0)'
+      $container.css({
+        transform: 'translate3d' + translate
+      })
+    })
+    $(window).on('mouseup pointerup touchup', function (event) {
+      if (!isDragging) {
+        return
+      }
+      isDragging = false
+      event.preventDefault()
+      var pos = $container.position()
+      $container.css({
+        top: pos.top,
+        left: pos.left,
+        transform: 'none'
+      })
+      if (localStorage) {
+        localStorage.YZWFEorzeaMapPos = pos.top + ',' + pos.left
+      }
+    })
   }
 
 })()
