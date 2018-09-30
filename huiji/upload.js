0617559ef013258b97e9e0be6708bd5471a67721
oott123 <git@public.oott123.com>
2018-09-30 20:48:36 +0800

    upload huiji data

--- huiji/upload.js
+++ huiji/upload.js
@@ -14,6 +14,19 @@ async function upload() {
   await updateGadget(bot, 'huiji/loader.css', 'EorzeaMapLoader.css')
   await updateGadget(bot, 'dist/map.css', 'EorzeaMap.css')
   await updateGadget(bot, 'dist/map.js', 'EorzeaMap.js')
+  await updateData(bot, 'generated/webroot/data/map.json', 'map.json', true)
+  await updateData(
+    bot,
+    'generated/webroot/data/mapMarker.json',
+    'mapMarker.json',
+    true
+  )
+  await updateData(
+    bot,
+    'generated/webroot/data/region.json',
+    'region.json',
+    true
+  )
 }
 
 async function updateGadget(bot, src, dest) {
@@ -25,6 +38,22 @@ async function updateGadget(bot, src, dest) {
     text: content,
     summary: '自动化脚本更新代码，有问题请联系 [[用户:云泽宛风]]',
     nocreate: true,
+    bot: true,
+    token: bot.editToken
+  })
+}
+
+async function updateData(bot, src, dest, wrap = false) {
+  console.log(`Updating ${dest} ...`)
+  let content = fs.readFileSync(src).toString()
+  if (wrap) {
+    content = JSON.stringify({ data: JSON.parse(content) })
+  }
+  await bot.request({
+    action: 'edit',
+    title: `Data:EorzeaMap/${dest}`,
+    text: content,
+    summary: '自动化脚本更新数据，有问题请联系 [[用户:云泽宛风]]',
     bot: true,
     token: bot.editToken
   })
